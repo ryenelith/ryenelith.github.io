f2385cef2aecfcc275d6596309d2bd1c6e3e1b88
ryenelith <ryenelith@vpn-global-dhcp2-211.ethz.ch>
2018-09-19 16:40:53 +0200

    removed all php, merger script to do includes, no view count

---
+++ snippets/merger.sh
@@ -0,0 +1,9 @@
+# index
+cat mainTop.html overview.html mainBottom.html > ../index.html
+
+# projects
+cat mainTop.html project_uni_1.html mainBottom.html > ../project_uni_1.html
+cat mainTop.html project_uni_2.html mainBottom.html > ../project_uni_2.html
+cat mainTop.html project_uni_3.html mainBottom.html > ../project_uni_3.html
+cat mainTop.html project_uni_4.html mainBottom.html > ../project_uni_4.html
+cat mainTop.html project_uni_5.html mainBottom.html > ../project_uni_5.html
